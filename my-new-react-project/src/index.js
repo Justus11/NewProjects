06cd9260640bdfb120c4458730bfb03a3226d965
Justus11 <57481057+Justus11@users.noreply.github.com>
2021-12-02 10:00:08 +0200

    added <Toggle/> button to index.js

--- my-new-react-project/src/index.js
+++ my-new-react-project/src/index.js
@@ -3,10 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const green = '#39D1B4';
+const yellow = '#FFD712';
 
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { color: green };
+    this.changeColor = this.changeColor.bind(this);
+  }
+
+  changeColor() {
+    const newColor = this.state.color == green ? yellow : green;
+    this.setState({ color: newColor });
+  }
+
+  render() {
+    return (
+      <div style={{ background: this.state.color }}>
+        <h1>
+          Change my color
+        </h1>
+        <button onClick={this.changeColor}>
+          Change color
+        </button>
+      </div>
+    );
+  }
+}
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Toggle />,<App />
   </React.StrictMode>,
   document.getElementById('root')
 );
